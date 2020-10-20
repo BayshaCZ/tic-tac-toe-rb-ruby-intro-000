f8958fade63ee89b3bb589a28381e8b716e21e8c
Eva Podbrdská <podbrdska@gmail.com>
2020-10-20 06:15:24 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -97,7 +97,6 @@ def play(board)
     turn(board)
   end
   if won?(board)
-    winner(board) == "X" || winner(board) == "O" 
     puts "Congratulations #{winner(board)}!"
   elsif draw?(board)
     puts "Cat\'s Game!"
